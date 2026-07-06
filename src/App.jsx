5ff78fc92827be49136998f74cc58f0da8c08517
Julia Rokhmatur Rizqi <juliarohmaturrizqi@gmail.com>
2026-07-07 06:10:06 +0700

    Up Tampilan Login

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    // Memanggil komponen Login untuk ditampilkan di layar
+    <Login />
+  );
+}
+
+export default App;
